6c1497ab6d8a76fd3c5cf844c25005f72b5e676b
3xoses <sebastian.staniak@interia.pl>
2020-03-29 19:55:54 +0200

    add social media card and show social media card after click contact button

--- main.js
+++ main.js
@@ -12,3 +12,24 @@ function showTime(){
 }
 
 showTime();
+
+//contact_button hover on click
+// show contact_bar on click contact_button
+
+const contactButtonHover = document.querySelector(".contact_button");
+const showContactBar = document.querySelector(".contact_bar");
+const addHover = function() {
+    contactButtonHover.style.borderLeft = "solid rgb(59, 59, 59) 2px"
+    contactButtonHover.style.borderTop = "solid rgb(59, 59, 59) 2px"
+    contactButtonHover.style.borderRight = "solid white 2px"
+    contactButtonHover.style.borderBottom = "solid white 2px"
+    showContactBar.style.display = "block"
+};
+// const removeHover = function() {
+//     contactButtonHover.style.borderLeft = "solid white 2px"
+//     contactButtonHover.style.borderTop = "solid white 2px"
+//     contactButtonHover.style.borderRight = "solid rgb(59, 59, 59) 2px"
+//     contactButtonHover.style.borderBottom = "solid rgb(59, 59, 59) 2px"
+// };
+
+contactButtonHover.addEventListener("click", addHover);
