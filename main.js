6aaf91cfd5f3430d7152b388478bfa506fa6dc4d
3xoses <sebastian.staniak@interia.pl>
2020-04-04 18:08:35 +0200

    add 0 in clock when minutes are <10

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ function showTime(){
     var date = new Date();
     var h = date.getHours(); // 0 - 23
     var m = date.getMinutes(); // 0 - 59
+    m = (m < 10) ? "0" + m : m;
     var time = h + ":" + m;
     document.getElementById("datetime").innerText = time;
     document.getElementById("datetime").textContent = time;
